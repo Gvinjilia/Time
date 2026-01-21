b3f148b4383b9a4211da17fbccfa8d8828674a6a
Gvinjilia <gvinjilian2@gmail.com>
2026-01-22 02:33:10 +0400

    updated soteria controller (code)

--- server/controllers/soteria.controller.js
+++ server/controllers/soteria.controller.js
@@ -1,12 +1,11 @@
 const Cart = require("../models/cart.model");
 const catchAsync = require("../utils/catchAsync");
 const Watch = require("../models/watch.model");
+const soteria = require('soteria-sdk');
+
+soteria.configure(process.env.SOTERIA_PUBLIC_KEY, process.env.SOTERIA_SECRET_KEY);
 
 const checkoutSession = catchAsync(async (req, res, next) => {
-    const { soteria } = await import("soteria-sdk");
-
-    soteria.configure(process.env.SOTERIA_PUBLIC_KEY, process.env.SOTERIA_SECRET_KEY);
-
     const cart = await Cart.findOne({ userId: req.user._id }).populate("cart.product");
 
     if(!cart || cart.cart.length === 0){
@@ -29,10 +28,6 @@ const checkoutSession = catchAsync(async (req, res, next) => {
 });
 
 const syncAllWatchesToSoteria = catchAsync(async (req, res, next) => {
-    const { soteria } = await import("soteria-sdk");
-    
-    soteria.configure(process.env.SOTERIA_PUBLIC_KEY, process.env.SOTERIA_SECRET_KEY);
-
     const allWatches = await Watch.find();
 
     const watches = allWatches.map((watch) => ({
